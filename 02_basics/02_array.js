3bd578fad737569856758eef3f3bb1d319ea65ef
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-07 20:54:59 +0530

    Array Part 2.

---
+++ 02_basics/02_array.js
@@ -0,0 +1,25 @@
+const marvel_heros = ["Thor", "IronMan", "Spiderman"]
+const dc_heros = ["Superman", "Flash", "Batman"]
+// marvel_heros.push(dc_heros) 
+
+// Push method in Array.
+
+// console.log(marvel_heros); // Here we can see that that the second array is added within the first array. 
+// console.log(marvel_heros[3][0]);
+
+// concat method. 
+
+// const all_heros = marvel_heros.concat(dc_heros);
+// console.log(all_heros);  
+
+// Spread Out Method. 
+
+// const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros);
+
+// Flat method or flat operator. 
+
+
+// const another_array = [1,2,3,[4,5,6,],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array); 
