3e8ce9858c5ef74d86ae200306b260cf440c477d
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-07 20:57:51 +0530

    Is Array, From Array, Of Array to be read again.

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -23,3 +23,5 @@ const dc_heros = ["Superman", "Flash", "Batman"]
 // const another_array = [1,2,3,[4,5,6,],7,[6,7,[4,5]]]
 // const real_another_array = another_array.flat(Infinity)
 // console.log(real_another_array); 
+
+// IS Array, From Array, of Array. 
