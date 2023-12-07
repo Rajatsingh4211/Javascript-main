3849950c5491e5e1b6d92971939d1ca0c8a87faf
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-07 23:59:14 +0530

    Object in Depth. Till Freeze.

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,26 @@
+// Singleton
+// Object.create => This method is used to create object as well. 
+// object literals. 
+
+const mySym = Symbol("Key1")
+const Jsuser = {
+    name : "Rajat", //By Deafault the system takes name as String. 
+    "full name" : "Rajat Kumar Singh",
+    [mySym]: "Key1",
+    age: 18, 
+    location: "Jaipur",
+    email: "hitesh@google.com", 
+    isloggedin: false,
+    lastloggedin: ["Monday", "Saturday"]
+
+}
+
+console.log(Jsuser.email);
+console.log(Jsuser["email"]);
+console.log(Jsuser["full name"]);
+Jsuser.email = "Hitesh@chatgpt.com"
+
+console.log(Jsuser);
+Object.freeze(Jsuser)
+Jsuser.email = 'hitesh@microsoft.com'
+console.log(Jsuser);
