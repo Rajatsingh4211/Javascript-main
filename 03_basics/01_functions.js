9280c8d120baea719b7ac30c2d8312d18ed804f0
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-08 19:31:35 +0530

    Function with Objects.

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,44 @@ function loginUserMessage (username){
   return `${username} just logged in `
 
 }
-console.log((loginUserMessage("Hitesh")));
+// console.log((loginUserMessage("Hitesh")));
+
+// FUNCTIONS WITH OBJECTS. 
+
+// function calculateCartPrice(num1){
+//  return num1 
+// }
+// console.log(calculateCartPrice(200, 400, 500));
+
+
+// REST OPERATOR
+function calculateCartPrice(...num1){ // Method to pass multiple operator in JS using spread method (...)
+  return num1 
+ }
+//  console.log(calculateCartPrice(200, 400, 500));
+
+
+const user = { 
+
+  username : "Hitesh",
+  price : 199
+
+}
+function handleOnject (anyobject){
+  //  console.log(`Username is ${anyobject.username}, and the price is ${anyobject.price}`);
+
+}
+// handleOnject(user)
+handleOnject(
+  {
+    username : "Sam",
+    Price : 399
+  }
+)
+
+const mynewArray = [200, 400, 300, 100]
+function returnSecondValue (getArray) { 
+return (getArray[2])
+}
+// console.log(returnSecondValue(mynewArray));
+console.log(returnSecondValue([200, 400, 500, 1000 ]));
