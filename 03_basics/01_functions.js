c8bb000b093cb8e842c5a57df27fbb29359c7fc0
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-13 22:51:24 +0530

    Functions with Objects.

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -23,3 +23,44 @@ function loginuserMessage(username){
 }
 
 // console.log(loginuserMessage("Rajat"));
+
+
+
+// FUNCTIONS IN JAVASCRIPT (FUNCTION WITH OBJECTS)
+
+function CalclulateCartPrice(...num1){
+
+return num1
+}
+
+// console.log(CalclulateCartPrice(200, 400, 500, 2000, 700))
+
+
+function CalclulateCartPrice2(val1, val2, ...num1){ //Hre we have given val11 and val2 which will take their
+  //respective values on their own.  
+   
+  return num1
+  }
+  
+  // console.log(CalclulateCartPrice2(200, 400, 500, 2000, 500))
+  
+
+  const user = {
+    username : "Hitesh", 
+    price : 299
+  }
+  function handleObject (anyObject){
+  console.log(`Username is ${anyObject.username}, and price is ${anyObject.price}`);
+  }
+  // handleObject(user)
+    // handleObject({
+    //   username : "sam", 
+    //   price : 399
+    // })
+
+    const newArray = [200, 400, 500]
+    function returnSecondArray (getArray) {
+     return getArray[1]
+    }
+    // console.log(returnSecondArray(newArray))
+    console.log(returnSecondArray(newArray))
