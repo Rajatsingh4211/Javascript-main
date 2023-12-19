a9643c0d273f0a3e75200f8a0e1fd2152382f9ae
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-19 16:48:05 +0530

    Iffe Modified

--- 03_basics/04_iif.js
+++ 03_basics/04_iif.js
@@ -1,4 +1,4 @@
-// Immediately invoked functions.
+// IMMEDIATELY INVOKED FUNCTION (IFFE)
 
 (function chai(){
     console.log(`Db connected`);
@@ -7,4 +7,15 @@
 (function aurcode(){
     console.log(`DB connectd 2`);
 }
+)();// Same here. 
+
+( () =>{
+    console.log(`DB connectd 3`);
+}
 )()
+
+ /* IIFE is used to wrap a fn in a small bracket () and then call the fn outside of it. So that it can help from 
+ the pollution of Global Scope.  Like in the above you can see that the fn is wraped in () and then it's 
+ called by another () which help the global scope from being polluted*/ 
+
+//  Fn can be written in anyway given above  
