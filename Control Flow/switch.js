c0f492dda874051a2eaa15140f02b16be5d0d68c
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-19 23:17:22 +0530

    Switch Statement.

---
+++ Control Flow/switch.js
@@ -0,0 +1,25 @@
+// SWITCH 
+
+const month = 4
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    
+        case 2:
+        console.log("Feb")
+        break;
+
+        case 3:
+        console.log("March")
+        break;
+
+        case 4:
+        console.log("April")
+        break;
+
+    default:
+        console.log("Default Case Match");
+        break;
+}
+// Shift + alt key + down Arrow => Used to copy and bring the code Down.  
