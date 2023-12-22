553d8d2c3a636a2a810c0afdecf41dffd937d0cc
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-22 21:05:04 +0530

    For In Array

---
+++ 05_iterations/four.js
@@ -0,0 +1,20 @@
+const myObj = {
+    js : "javascript", 
+    cpp : "C++",
+    rb : "Ruby", 
+    swift : "Swift by Apple", 
+
+}
+
+for (const key in myObj) {
+// console.log(myObj [key]);
+// or 
+// console.log(`${key} is for ${myObj [key]}`);
+}
+
+// forIn FOR ARRAY. 
+
+const programming = ["js", "cpp", "swift", "java", "python"]
+for (const key2 in programming) {
+   console.log(programming[key2]);
+}
