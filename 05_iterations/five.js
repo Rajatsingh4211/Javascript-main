082833c5e21d43d6f8330efdcd3fdb73556c6930
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-22 21:05:36 +0530

    For Each in Array.

---
+++ 05_iterations/five.js
@@ -0,0 +1,40 @@
+const coding = ["js", "cpp", "swift", "java", "python"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// }  )
+
+// coding.forEach( (anyvalue)=> {
+
+//     console.log(anyvalue);
+// })
+
+// function printme(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printme)
+
+coding.forEach((item, index, arr) => {
+
+    // console.log(item, index, arr);
+})
+
+const Mycoding = [
+    {
+        languageName : "JavaScript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "Java",
+        languageFileName : "Java"
+    },
+    {
+        languageName : "Python",
+        languageFileName : "Py"
+    }
+]
+
+Mycoding.forEach ( (item)=> {
+    console.log(item.languageName);
+})
