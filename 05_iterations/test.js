8633fe6097de89a771dd47cb7d45ad10e78a5ea7
Rajat Kumar Singh <rajatsingh4211@gmail.com>
2023-12-26 22:37:55 +0530

    Properties of Array.

--- 05_iterations/test.js
+++ 05_iterations/test.js
@@ -14,4 +14,95 @@ class Animal{
 let dog = new Animal("dog", 4, "Bhow Bhow");
 let Cat = new Animal("cat", 4, "Meow Meow");
 
-Cat.Speak();
+// Cat.Speak();
+
+// 
+
+const characters = [
+    {
+        name : "Tarak Mehta", 
+        height : "172",
+        mass : 60,
+        gender: "Male"
+    },
+    {
+        name : 'Jethalal',
+        height : "160",
+        mass : 110,
+        gender: "Male"
+    },
+    {
+        name : "ChampakLal",
+        height : "169",
+        mass : 90,
+        gender: "male",
+    },
+    {
+        name : "bide",
+        height : "171",
+        mass : 111,
+        gender: "male"
+    }
+]
+
+
+
+// //Get array of All Names. 
+// // const names = characters.map(ch =>ch.name)
+// const names = characters.map(ch=> ch.name)
+// // console.log(names);
+
+// // Get an array of objects with just name and height properties. 
+// const propertiesofHeights = characters.map(ch=>{
+//     return {names: ch.name, height: ch.height}
+// })
+
+// // console.log(propertiesofHeights);
+
+// Get the total height of All characters. 
+
+const totalHeight = characters.reduce((prevHeight, characters)=>{
+
+return prevHeight + Number(characters.height)
+
+},0)
+// console.log(totalHeight);
+
+
+// const greaterThanMass = characters.filter((characters)=>{
+// return characters.mass > 100
+
+// })
+
+// const greaterThanMass = characters.filter((characters) => {
+//     return characters.mass > 100;
+// });
+
+// console.log(greaterThanMass);
+
+const greaterThanMass = characters.filter((character) => {
+    return character.mass > 100;
+});
+
+// console.log(greaterThanMass);
+
+
+// Get all male characters. 
+
+const genderFilter = characters.filter((character)=>{
+return character.gender>male; 
+})
+
+// console.log(genderFilter);
+
+
+class Animal {
+    constructor(name, legCount) {
+      this.name = name
+      this.legCount = legCount
+    }
+    describe() {
+      return `${this.name} has ${this.legCount} legs`
+    }
+  }
+  
